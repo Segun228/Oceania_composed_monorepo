1c807979cfc390e26f14c3bcecb39861b2f1f827
Segun228 <segunperkele@gmail.com>
2025-08-30 15:35:41 +0300

    adding docker compose

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,12 @@
 import os
 import logging
-from contextlib import asynccontextmanager
+import asyncio
 
-from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher, types
+from dotenv import load_dotenv
 
 from app.handlers.router import admin_router, user_router
 from app.middlewares.antiflud import ThrottlingMiddleware
-from dotenv import load_dotenv
 
 import app.handlers.admin_handlers
 import app.handlers.user_handlers
@@ -16,15 +15,6 @@ load_dotenv()
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-
-WEBHOOK_PATH = "/webhook"
-RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
-if not RENDER_EXTERNAL_HOSTNAME:
-    logging.warning("RENDER_EXTERNAL_HOSTNAME is not set. Webhook URL might be incorrect on local run (e.g., if using ngrok).")
-    WEBHOOK_URL = f"https://oceanica-bot.onrender.com{WEBHOOK_PATH}"
-else:
-    WEBHOOK_URL = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}{WEBHOOK_PATH}"
 if not BOT_TOKEN:
     logging.error("No token provided")
     raise ValueError("No token provided")
@@ -35,31 +25,13 @@ dp.message.middleware(ThrottlingMiddleware(limit=0.5))
 dp.include_router(admin_router)
 dp.include_router(user_router)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await bot.delete_webhook(drop_pending_updates=True)
-    await bot.set_webhook(WEBHOOK_URL)
-    logging.info(f"Webhook set to {WEBHOOK_URL}")
-    yield
-
-    logging.info("Shutting down bot...")
-    await bot.delete_webhook()
-    await bot.session.close()
-
-app = FastAPI(lifespan=lifespan)
-
-@app.post(WEBHOOK_PATH)
-async def webhook(request: Request):
-    update = types.Update(**await request.json())
-    await dp.feed_update(bot=bot, update=update)
-    return {"ok": True}
-
-@app.get("/")
-async def health():
-    return {"status": "ok"}
+async def main():
+    logging.info("Starting bot with long polling...")
+    try:
+        await dp.start_polling(bot)
+    finally:
+        logging.info("Shutting down bot...")
+        await bot.session.close()
 
 if __name__ == "__main__":
-    import uvicorn
-    logging.info("Main started")
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
-    logging.info("Host started")
+    asyncio.run(main())
